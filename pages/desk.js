19d8920d4b160a31ce5cefc64f537ebb78ceff55
MERT CAN DÜLDÜL <mertcanduldul@gmail.com>
2021-10-23 22:14:21 +0300

    desk section added

--- pages/desk.js
+++ pages/desk.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Box } from "@chakra-ui/layout";
+import { Container, Text } from "@nextui-org/react";
 
-export default function desk({children}) {
+
+export default function desk({ children }) {
     return (
-        <div>a
-            {children}
-        </div>
+        <Container>
+            <Box bg="orange.200">
+                <Box textAlign="center">
+                    <Text size={40} style={{ fontFamily: 'Courier', textDecoration: 'underline' }}>MASAM</Text>
+                    <Text size={16} style={{ fontFamily: 'Courier' }}>
+                        Equipment that I use a lot in my daily life</Text>
+                </Box>
+            </Box>
+        </Container>
     )
 }
